9102ab564e11bcfc80400be6c192105fc4c68b7f
z3nology <tardis.abs@gmail.com>
2024-05-06 12:30:07 -0700

    fix: change the social links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -109,7 +109,7 @@ const Footer = () => {
 
         <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
         <div className="mt-5 flex items-center justify-center gap-4">
-          <Link href="https://discord.gg/tekionft">
+          <Link href="https://x.com/tekionft?s=21&t=lLJnFEhSsk6SZUBDdGZTcA">
             <div className="cursor-pointer text-gray-600 transition-all duration-300 hover:text-blue-400 dark:text-white dark:hover:text-blue-400">
               <FaXTwitter size={25} />
             </div>
@@ -119,7 +119,7 @@ const Footer = () => {
               <FaTelegramPlane size={25} />
             </div>
           </Link>
-          <Link href="https://x.com/tekionft?s=21&t=lLJnFEhSsk6SZUBDdGZTcA">
+          <Link href="https://discord.gg/tekionft">
             <div className="cursor-pointer text-gray-600 transition-all duration-300 hover:text-blue-400 dark:text-white dark:hover:text-blue-400">
               <FaDiscord size={25} />
             </div>
